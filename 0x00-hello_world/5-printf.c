ad5bf18b7bcbfe871be567403c99e4385a2e1031
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-08-11 10:19:38 +0300

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-int main(void){
+ * main - displays "with proper grammar" with new line
+ * Return: 0 and exit the program
+ */
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - display the size of data types
+ * Return: 0 and exit the program.
+ */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
@@ -11,5 +11,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return(0);
+	return (0);
 }
