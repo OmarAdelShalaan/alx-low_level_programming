1cc0439b8e142634d5dc945b475b8b4a9f322ccd
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-08-11 09:58:53 +0300

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,6 @@
 * Return: Always 0 (Success)
 */
 int main(void){  
-	/* print  */
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
