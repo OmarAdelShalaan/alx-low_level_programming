b502e60b043bdbf99f19eac9d43a06f5059b01bd
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-08-15 14:56:22 +0300

    C

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,14 +11,3 @@ int main(void)
     print_alphabet();
     return (0);
 }
-
-void print_alphabet(void)
-{
-	int i = 0;
-
-	for (i = 0; i < 26; ++i)
-	{
-		printf("%c", i + 'a');
-	}
-	printf("\n");
-}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,10 @@
+void print_alphabet(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 26; ++i)
+	{
+		printf("%c", i + 'a');
+	}
+	printf("\n");
+}
