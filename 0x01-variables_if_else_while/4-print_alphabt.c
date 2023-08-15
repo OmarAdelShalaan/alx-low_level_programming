bc607b2db766da039f8427ebbd40858bf8a9a2c5
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-08-15 13:49:11 +0300

    C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 26; ++i)
+	{
+		printf("%c", i + 'a');
+	}
+	printf("\n");
+	return (0);
+}
