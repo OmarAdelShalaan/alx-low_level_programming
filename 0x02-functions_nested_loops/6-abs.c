656654b2039188ee5c978b9d7c8215715093b3dd
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-08-17 02:43:02 +0300

    c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,26 +1,12 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a nummber
- * @n: number
- *
- * Return: 1 and '+', 0 and 0, -1 '-' based on a condition
+ * _abs - prints absolute value of a number
+ * @c: Number
+ * Return: ABSOLUTE VALUE
  */
-int print_sign(int n)
+int _abs(int c)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+	return (abs(c));
 }
