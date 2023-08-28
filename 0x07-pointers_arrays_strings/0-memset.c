515ca74b9dffa07ca5961c426c550cefa93428f9
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-08-28 22:08:55 +0300

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - is used to fill a block of memory with a particular value.
+ * @s: the address of memory
+ * @b: char will input in memory
+ * @n: number of element will replace
+ * Return: pointer.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return s;
+}
