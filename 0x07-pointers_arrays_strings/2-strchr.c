b80c46481ec754bee51bdf40260f6c9f87888593
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-08-28 22:41:34 +0300

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - function that locates a character in a string.
+ * @s: the address of memory
+ * @c: char will input in memory
+ * Return: pointer.
+ */
+char *_strchr(char *s, char c)
+{
+	s[i] = c;
+	return s;
+}
