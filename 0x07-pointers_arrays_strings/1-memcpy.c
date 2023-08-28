cb07388bc7a12247744e8e4ad3664bc1788b1fd3
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-08-28 22:36:54 +0300

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - is used to copy a block of memory from one location to another. It is declared in <string.h> header file.
+ * @dest: A pointer to the memory location where the copied data will be stored.
+ * @src: A pointer to the memory location from where the data is to be copied.
+ * @n: number of element will copied
+ * Return: pointer.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return s;
+}
