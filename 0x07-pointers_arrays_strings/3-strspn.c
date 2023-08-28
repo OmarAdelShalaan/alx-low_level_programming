f616074923dcd41386e73088f0b586956051ccef
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-08-28 23:10:11 +0300

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - function returns the length of the initial substring of the string pointed to by str1 that is made up of only those character contained in the string pointed to by str2.
+ * @s: the address of memory
+ * @accept: char will input in memory
+ * Return: pointer.
+ */
+ 
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0, count = 0;
+	
+	while (accept[j] != '\0')
+	{
+		while (s[i] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				break;
+			}
+			i++;
+		}
+		j++;
+	}
+	return count;
+}
