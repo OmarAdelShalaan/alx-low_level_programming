f398a2260c41f165f46591e491ef4449ab3a4c62
OmarAdelShalaan <omaradelshalaan@gmail.com>
2023-08-31 03:17:59 +0300

    0x08. C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,15 +6,15 @@
  * @s : string
  * Return: void
  */
- 
+
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-    	printf("\n");
-    	return ;
-    }
-    printf("%c",*s);
-    _puts_recursion(++s);
+	if (*s == '\0')
+	{
+		printf("\n");
+		return ;
+	}
+	printf ("%c",*s);
+	_puts_recursion(++s);
 }
 
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - that returns the value of x raised to the power of y.
+ * @x : integer number
+ * @y : integer power number
+ * Return: integer power of number.
+ */
+ 
+int _pow_recursion(int x, int y)
+{
+    //static int powResult = 1;
+    if (y == 1)
+    {
+        return x;
+    }
+    esle if (y < 0)
+    {
+    	return -1;
+    }
+    //powResult *= _pow_recursion (x, --y);
+    return x * _pow_recursion(x, --y);
+}
+
