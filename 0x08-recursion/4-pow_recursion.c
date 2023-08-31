34ce568ff2d1a2e79387f29d48ddda6efa05f77b
OmarAdelShalaan <omaradelshalaan@gmail.com>
2023-08-31 03:47:53 +0300

    0x08. C - Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,16 +10,14 @@
 
 int _pow_recursion(int x, int y)
 {
-    //static int powResult = 1;
-    if (y == 1)
-    {
-        return x;
-    }
-    esle if (y < 0)
-    {
-    	return -1;
-    }
-    //powResult *= _pow_recursion (x, --y);
-    return x * _pow_recursion(x, --y);
+	if (y == 1)
+	{
+		return x;
+	}
+	esle if (y < 0)
+	{
+		return -1;
+	}
+	return x * _pow_recursion(x, --y);
 }
 
