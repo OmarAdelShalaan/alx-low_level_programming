6c04b331c6d1a4bbeffae2e0e41b61cdfac6b3cb
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-09-05 02:22:27 +0300

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - that  returns a pointer to s1, followed by the contents of s2
+ * @s1 : string
+ * @s2 : string
+ * Return: char *.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, count = 0;
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+		count++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		i++;
+		count++;
+	}
+	char *newStr = (char *)malloc(sizeof(char) * ((count * 2) - 1));
+
+	if (newStr == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		newStr[count] = s1[i];
+		i++;
+		count++;
+	}
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		newStr[count] = s2[i];
+		i++;
+		count++;
+	}
+	newStr[count] = '\0';
+	free(s1);
+	free(s2);
+	return (newStr);
+}
