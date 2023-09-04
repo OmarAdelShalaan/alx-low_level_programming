0161637a9e9fed2496477c7cde7140d0bf65adf0
OmarAdelShalaan <omaradelshalaan@gmail.com>
2023-09-04 15:41:10 +0300

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print_line - draws straight line
+ * @n: number of times line should be printed
+ *
+ * Return: Straight line
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0; 
+}
