3d7da32cdf286ac7d0b1cec8677006d38b694dba
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-09-05 02:58:47 +0300

    0x0B-malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	unsigned int i, count = 0;
-	char *newStr = (char *)malloc(sizeof(char) * 100);
+	char *newStr = (char *)malloc(sizeof(char) * 50);
 
 	i = 0;
 	while (s1[i] != '\0')
