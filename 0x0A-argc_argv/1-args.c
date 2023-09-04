7d962c12915db04c8a2443e87ae3c05d37ef4b69
OmarAdelShalaan <omaradelshalaan@gmail.com>
2023-09-04 15:51:52 +0300

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main fun start code from it
+ * @argc : number of command arguments
+ * @argv : strings input in command arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return 0;
+}
+
