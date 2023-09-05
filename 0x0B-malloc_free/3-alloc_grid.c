3819f3fd06abe7e1bae2983f979919dc8d3efb32
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-09-05 06:10:51 +0300

    0x0B-malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,6 +21,6 @@ int **alloc_grid(int width, int height)
 	grid = (int **)malloc(sizeof(int) * width * height);
 	for(i = 0; i < height; i++)
 		for(j = 0; j < width; j++)
-			grid[i][j] = 0;
+			grid[i*width][j] = 0;
 	return (grid);
 }
