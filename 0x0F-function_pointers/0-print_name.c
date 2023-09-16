dba06c2e7c3aa616ee698a7431fb9379be253cdf
OmarAdelShalaan <omaradelshalaan@gmail.com>
2023-09-16 18:34:08 +0300

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+
+/**
+ * print_name - that print the name
+ * @name : string 
+ * @pointer to fun : input char * return void
+ * Return: void.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
+
