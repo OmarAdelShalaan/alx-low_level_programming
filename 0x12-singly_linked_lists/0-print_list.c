1e705dd2439b3ce70139be3d0accf8b4b8f43e36
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-09-25 22:43:45 +0300

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - that prints all the elements of a list_t list.
+ * @h : List will printed
+ * Return: int.
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h->next != NULL)
+	{
+		printf("%s\n", h->str);
+		h = h->next;
+	}		
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+# alx-low_level_programming
+
+## 0x12-singly_linked_lists
