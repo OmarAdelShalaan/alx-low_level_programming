7fc32577a600964282fccb629327a3ac131b9da7
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-12-19 01:03:03 +0200

    0x15

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,35 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX stout
- * @filename: pointer to the name of the file
- * @letters: the number of letters it should read and print
- * Return: the actual number of letters it could read and print
+ * read_textfile - Reads a text file and
+ * printd it to the POSIX
+ * @filename: name of file to be read from
+ * @letters: number of chars to read
+ *
+ * Return: ssize_t
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t o, r, w;
-    char *buf = NULL;
+	ssize_t o, r, w;
+	char *buf;
 
-    if (filename == NULL)
-    {
-        return (0);
-    }
-    buf = (char *)malloc(letters);
-    if (buf == NULL)
+	if (filename == NULL)
 		return (0);
-    o = open(filename, O_RDONLY);
-    r = read(o, buf, letters);
-    w = write(STDOUT_FILENO, buf, r);
-    free(buf);
-    if (r == -1 || w == -1 || o == -1)
-    {
-        return (0);
-    }
-    close(o);
-    return (w);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
+
+	if (o == -1 || r == -1 || w == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	close(o);
+	return (w);
 }
